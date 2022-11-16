d3cb3d13e228ddadf9c8df3a5b758b6e71e5aff7
khbminus <khbminus@gmail.com>
2022-11-16 18:56:22 +0100

    Tests

---
+++ test/Test/Parsers.hs
@@ -0,0 +1,40 @@
+module Test.Parsers where
+
+import Text.Megaparsec
+import Grammar
+import Statement
+
+import Test.HUnit
+
+parseSuccessful :: Eq a => Parser a -> String -> a -> Bool
+parseSuccessful parser line result = case parse parser "" line of
+  Left _ -> False
+  Right a -> a == result
+
+parseFailed :: Parser a -> String -> Bool
+parseFailed parser line = case parse parser "" line of
+  Left _ -> True
+  Right _ -> False
+
+
+unit_parser_const :: IO ()
+unit_parser_const = do
+  let succConst = parseSuccessful pInteger
+  let failConst = parseFailed pInteger
+  
+  assertBool "const parser failed" $ succConst "1" (Const 1)
+  assertBool "const parser failed" $ succConst "1.23456" (Const 1)
+  assertBool "const parser failed" $ succConst "1234567" (Const 1234567)
+  assertBool "const parser failed" $ failConst "ahahahahh1234"
+
+unit_parser_var_name :: IO ()
+unit_parser_var_name = do
+  let succVar = parseSuccessful pVarName
+  let failVar = parseFailed pVarName
+
+  assertBool "var parser failed" $ failVar "1234abc"
+  assertBool "var parser failed" $ failVar ""
+  assertBool "var parser failed" $ succVar "abcd" (VariableName "abcd")
+  assertBool "var parser failed" $ succVar "a1234" (VariableName "a1234")
+
+
