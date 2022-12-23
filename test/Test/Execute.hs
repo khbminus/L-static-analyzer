1b289b3c145aee6ce8de172404e0d653c6f98793
khbminus <khbminus@gmail.com>
2022-12-23 10:55:14 +0100

    Fun functions tests

--- test/Test/Execute.hs
+++ test/Test/Execute.hs
@@ -1,16 +1,15 @@
-{-# LANGUAGE LambdaCase #-}
 module Test.Execute where
 
-import Test.Tasty.HUnit (assertEqual, assertBool)
-import Statement (Expression(..), Statement (..))
+import Context (Buffer (..), Context (..), VarContext (..), newContext)
+import Control.Monad.State (execStateT)
 import qualified Data.Map as Map
-import Context (Context(..), setVar, setError, VarContext (..), Buffer(..), newContext)
+import Error (RuntimeError (..))
 import Evaluate (evaluateStatements)
-import Control.Monad.State ( evalStateT, execStateT )
-import Error (RuntimeError(..))
-import qualified Data.Map as Map
 import GHC.IO.Handle (hClose)
 import GHC.IO.Handle.FD (stdin)
+import Statement (Expression (..), Operations (..), Statement (..))
+import Test.Tasty.HUnit (assertBool)
+import Execute(run)
 
 checkOutput :: Context -> [String] -> Bool
 checkOutput cxt out = Context.output cxt == Buffer out
@@ -18,38 +17,91 @@ checkOutput cxt out = Context.output cxt == Buffer out
 checkError :: Context -> RuntimeError -> Bool
 checkError cxt err = Context.error cxt == Just err
 
-noFlushContext = newContext { flushEnabled = False }
+noFlushContext :: Context
+noFlushContext = newContext {flushEnabled = False}
 
 unit_executeWrite :: IO ()
 unit_executeWrite = do
-   let writeConst = Write (Const 1)
-   let writeVar = Write (VariableName "var")
-   let contextWithVar = noFlushContext { vars = [VarContext (Map.fromList [("var", 123)])] } 
+  let writeConst = Write (Const 1)
+  let writeVar = Write (VariableName "var")
+  let contextWithVar = noFlushContext {vars = [VarContext (Map.fromList [("var", 123)])]}
 
-   hClose stdin
+  hClose stdin
 
-   exitContext <- execStateT (evaluateStatements [writeConst]) noFlushContext
-   assertBool "write const" $ checkOutput exitContext ["1"]
+  exitContext <- execStateT (evaluateStatements [writeConst]) noFlushContext
+  assertBool "write const" $ checkOutput exitContext ["1"]
 
-   exitContext <- execStateT (evaluateStatements [writeVar]) contextWithVar
-   assertBool "write var" $ checkOutput exitContext ["123"]
+  exitContext <- execStateT (evaluateStatements [writeVar]) contextWithVar
+  assertBool "write var" $ checkOutput exitContext ["123"]
 
-   exitContext <- execStateT (evaluateStatements [writeVar]) noFlushContext
-   assertBool "write var failure" $ checkOutput exitContext []
-   assertBool "write var failure" $ checkError exitContext (VarNotFound "var")
+  exitContext <- execStateT (evaluateStatements [writeVar]) noFlushContext
+  assertBool "write var failure" $ checkOutput exitContext []
+  assertBool "write var failure" $ checkError exitContext (VarNotFound "var")
 
 unit_executeRead :: IO ()
 unit_executeRead = do
-   let readVar = Read "var"
-   let writeConst = Write (Const 1)
-   let writeVar = Write (VariableName "var")
-   let contextWithInput = noFlushContext { input = Buffer ["123"]}
+  let readVar = Read "var"
+  let writeConst = Write (Const 1)
+  let writeVar = Write (VariableName "var")
+  let contextWithInput = noFlushContext {input = Buffer ["123"]}
 
-   hClose stdin
+  hClose stdin
 
-   exitContext <- execStateT (evaluateStatements [readVar, writeVar]) contextWithInput
-   assertBool "read var success" $ checkOutput exitContext ["123"]
+  exitContext <- execStateT (evaluateStatements [readVar, writeVar]) contextWithInput
+  assertBool "read var success" $ checkOutput exitContext ["123"]
 
-   exitContext <- execStateT (evaluateStatements [readVar]) noFlushContext
-   assertBool "read var failure: end of input" $ checkError exitContext UnexpectedEOF
+  exitContext <- execStateT (evaluateStatements [readVar]) noFlushContext
+  assertBool "read var failure: end of input" $ checkError exitContext UnexpectedEOF
 
+unit_basicWhileTest :: IO ()
+unit_basicWhileTest = do
+  -- let code = "x := 1\n" ++ "write x + 10\n" ++ "while x > 0 do write x; x := x - 1" ++ "write x"
+  let code =
+        [ Let "x" (Const 1),
+          Write $ Application $ Addition (VariableName "x") (Const 10),
+          While
+            (Application $ Greater (VariableName "x") (Const 0))
+            [Write $ VariableName "x", Let "x" (Application $ Subtraction (VariableName "x") (Const 1))],
+          Write $ VariableName "x"
+        ]
+  let context = noFlushContext {input = Buffer []}
+
+  hClose stdin
+  exitContext <- execStateT (evaluateStatements code) context
+  assertBool "test successfull" $ checkOutput exitContext ["11", "1", "0"]
+
+unit_functions :: IO ()
+unit_functions = do
+  let code = 
+        [
+          "def f() { write 1 } return 2", 
+          "f()", 
+          "f()", 
+          "write f() + 2", 
+          "x := 1", 
+          "def g() { write x }", 
+          "g()", 
+          "def h() { y := x; write y; write x; x := x + 10; write x } return x",
+          "write h()",
+          "write x",
+          "def sum(a, b) { skip } return a + b",
+          "write sum(3, 4)"
+        ]
+  let context = noFlushContext {input = Buffer []}
+  
+  hClose stdin
+  evaluated <- execStateT (run code) context
+  assertBool "function declaration works" $ checkOutput evaluated [
+    "1", 
+    "1", 
+    "1", 
+    "4",
+    "1",
+    "1",
+    "1",
+    "11",
+    "11",
+    "1",
+    "7"
+    ]
+  
